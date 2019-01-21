f9f68000d3a2f74435f9bf5c7fbd25aad348e6b2
1amchris <beaulieu.christophe23@gmail.com>
2019-01-21 09:44:10 -0500

    Added Sudoku_generator_solution.sln

---
+++ sudoku_generator_solution/sudoku_generator_solution/main.cpp
@@ -0,0 +1,22 @@
+/*
+	Sudoku/sudoku_generator_solution/sudoku_generator_solution/main.cpp
+	Created by	Beaulieu,	Christophe
+
+	Goal :	The goal of this program is to generate random sudoku of random difficulties.
+			To be valid, each sudoku must:
+				- follow Sudoku rules
+				- have a unique solution
+			The sudokus can be saved in an existing json file or create a new one
+
+	Date : 2019-01-21
+*/
+
+#include <iostream>
+#define PAUSE system("pause")
+
+
+int main() {
+
+	PAUSE;
+	return 0;
+}
